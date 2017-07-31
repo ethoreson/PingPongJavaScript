148b99ded05d7cf5c35babf2cc07416443c3e0d5
Emilie Thoreson <thoresonemilie@gmail.com>
2017-07-31 09:21:53 -0700

    build pingpong back and front end

---
+++ js/pingpong-interface.js
@@ -0,0 +1,23 @@
+var Calculator = require('./../js/pingpong.js').calculatorModule;
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+    var goal = $('#goal').val();
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(goal);
+    output.forEach(function(element) {
+      $('#solution').append("<li>" + element + "</li>");
+    });
+  });
+
+  $('#math-operation').submit(function(event) {
+    event.preventDefault();
+    var x = $('#x').val();
+    var sign = $('#sign').val();
+    var y = $('#y').val();
+    var simpleCalculator = new Calculator("hot pink");
+    var output = simpleCalculator.pingPong(x, sign, y);
+      $('#calculatorSolution').append(output);
+  })
+});
---
+++ js/pingpong.js
@@ -0,0 +1,36 @@
+function Calculator(skinName) {
+  this.skin = skinName;
+}
+
+Calculator.prototype.pingPong = function(goal) {
+  var output = [];
+  for (var i = 1; i <= goal; i++) {
+    if (i % 15 === 0) {
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
+    } else  {
+      output.push(i);
+    }
+  }
+  return output;
+};
+
+Calculator.prototype.operation = function(xx, sign, yy) {
+  var x = parseInt(xx);
+  var y parseInt(yy);
+  if (sign === "+") {
+    return (x + y);
+  } else if (sign === "-") {
+    return (x - y);
+  } else if (sign === "*") {
+    return (x * y);
+  } else {
+    return (x / y);
+  }
+}
+
+
+exports.calculatorModule = Calculator;
